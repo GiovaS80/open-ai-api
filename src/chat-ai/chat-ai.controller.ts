0570a5cfd7f0cb21a52e6b9e3e42b97a9a4e1227
Gio <giomain.pv@gmail.com>
2023-07-06 14:17:59 +0700

    add Get listModel

--- src/chat-ai/chat-ai.controller.ts
+++ src/chat-ai/chat-ai.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ChatAiService } from './chat-ai.service';
 import { GetAiModelAnswer } from './model/get-ai-model-answer';
 
 @Controller('chat-ai')
 export class ChatAiController {
-    constructor(private readonly service:ChatAiService){}
+    constructor(private readonly service: ChatAiService) { }
 
     @Post("/message")
-    getModelAnswer(@Body() data:GetAiModelAnswer){
+    getModelAnswer(@Body() data: GetAiModelAnswer) {
         return this.service.getModelAnswer(data.question)
     }//end getModelAnswer
+
+    @Get("/model")
+    listModels() {
+        return this.service.listModels()
+    }//end listModels
+
 }//end class ChatAiController
--- src/chat-ai/chat-ai.service.ts
+++ src/chat-ai/chat-ai.service.ts
@@ -6,8 +6,10 @@ const DEFAULT_TEMPERATURE = 0.9
 
 @Injectable()
 export class ChatAiService {
-    private readonly openAiApi:OpenAIApi
-    constructor(){
+    private readonly openAiApi: OpenAIApi
+    private selectedModelId: string | undefined
+
+    constructor() {
         const configuration = new Configuration({
             organization: process.env.ORGANIZATION_ID,
             apiKey: process.env.OPENAI_API_KEY,
@@ -16,21 +18,33 @@ export class ChatAiService {
         this.openAiApi = new OpenAIApi(configuration);
     }//end constructor
 
-    async getModelAnswer(question:string,temperature?:number){
+    setModelId(modelId: string) {
+        if(modelId.includes(":")){
+            modelId = modelId.replace(":","-")
+        }
+        this.selectedModelId = modelId 
+    }//end setModelId
+
+    async listModels(){
+        const models = await this.openAiApi.listModels()
+        return models.data
+    }//end listModels
+
+    async getModelAnswer(question: string, temperature?: number) {
         try {
-            const params:CreateCompletionRequest = {
-                prompt:question,
-                model:DEFAULT_MODEL_ID,
-                temperature:temperature!=undefined?temperature:DEFAULT_TEMPERATURE
+            const params: CreateCompletionRequest = {
+                prompt: question,
+                model: this.selectedModelId ? this.selectedModelId : DEFAULT_MODEL_ID,
+                temperature: temperature != undefined ? temperature : DEFAULT_TEMPERATURE
             }
             const response = await this.openAiApi.createCompletion(params)
-            
-            const {data} = response
-            if(data.choices.length){return data.choices}
+
+            const { data } = response
+            if (data.choices.length) { return data.choices }
 
             return response.data
         } catch (error) {
-            
+
         }
     }//end getModelAnswer
 
